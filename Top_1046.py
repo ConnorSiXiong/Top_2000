757eeb9abb6238d194ec59f1ad9f8268062a4f7c
AlexSiXiong <2428545784@qq.com>
2020-04-14 00:42:48 +1200

    Q.153 classic list binary search

--- Top_1046.py
+++ Top_1046.py
@@ -1,9 +1,13 @@
+import bisect
 import heapq
+
 from typing import List
+
 
 def lastStoneWeight(self, stones: List[int]) -> int:
     heap = [-x for x in stones]
     heapq.heapify(heap)
+
     while len(heap) > 1:
         big = heapq.heappop(heap)
         small = heapq.heappop(heap)
@@ -12,13 +16,29 @@ def lastStoneWeight(self, stones: List[int]) -> int:
 
         if new_weight != 0:
             heapq.heappush(heap, new_weight)
+
     if len(heap):
         return -heap[0]
     else:
         return 0
 
 
-a = [9,1,2,5,3,4]
+def lastStoneWeight2(self, A):
+    h = [-x for x in A]
+    heapq.heapify(h)
+    while len(h) > 1 and h[0] != 0:
+        heapq.heappush(h, heapq.heappop(h) - heapq.heappop(h))
+    return -h[0]
+
+
+def lastStoneWeight3(self, A):
+    A.sort()
+    while len(A) > 1:
+        bisect.insort(A, A.pop() - A.pop())
+    return A[0]
+
+
+a = [9, 1, 2, 5, 3, 4]
 print(a)
 
 heap = [-x for x in a]
@@ -26,5 +46,11 @@ heapq.heapify(heap)
 
 print(heap)
 
+
 for i in range(len(a)):
-    print(heapq.heappop(heap))
+    if i == 0:
+        print('[', heapq.heappop(heap), sep='', end=', ')
+    elif i != len(a) - 1:
+        print(heapq.heappop(heap), end=', ')
+    else:
+        print(heapq.heappop(heap), end='] ')
---
+++ Top_153.py
@@ -0,0 +1,53 @@
+from typing import List
+
+
+def findMin(nums: List[int]) -> int:
+
+    for i in range(1, len(nums)):
+        if nums[i] < nums [i - 1]:
+            return nums[i]
+    return nums[0]
+
+
+# 最小值在 mid 的右侧
+# [4,5,6,7,0,1,2]
+
+# 最小值在 mid 的左侧
+# [6,7,0,1,2,3,4]
+
+def findMin2(nums: List[int]) -> int:
+    """
+    这个解法跟其他的解法不一样
+    它总是在比较nums[-1]
+    而不是拿中间值去比较
+    """
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        print('mid ', mid)
+        if nums[mid] <= nums[-1]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return nums[left]
+
+
+def findMin3(nums: List[int]) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+
+        mid = (left + right) // 2
+        print('mid ', mid)
+        if nums[mid] <= nums[right]:
+            right = mid
+        else:
+            left = mid + 1
+        # if nums[mid] > nums[right]:
+        #     left = mid + 1
+        # else:
+        #     right = mid
+    return nums[left]
