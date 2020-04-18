d27f466e38e1ea023f3f238136809a0c786cbbda
AlexSiXiong <2428545784@qq.com>
2020-04-19 00:17:43 +1200

    a classic DP problem

---
+++ Top_64_DP.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+def minPathSum(grid: List[List[int]]) -> int:
+    row = len(grid)
+    col = len(grid[0])
+
+    for i in range(1, row):
+        grid[i][0] += grid[i-1][0]
+
+    for i in range(1, col):
+        grid[0][i] += grid[0][i-1]
+
+    for i in range(1, row):
+        for j in range(1, col):
+            grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+
+    return grid[-1][-1]
+
+
+a = [
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+
+print(minPathSum(a))
