dbedb4f632e8c3fab4f4c423b5ad6ad16cd981c2
AlexSiXiong <2428545784@qq.com>
2020-05-01 23:54:46 +1200

    Aprial-30

---
+++ Challenges_April/Aprial_30.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidSequence(self, root: TreeNode, arr: List[int]) -> bool:
+        def dfs(root, index):
+            arr_val = arr[index]
+
+            if root is None or root.val != arr_val:
+                return False
+
+            if index == len(arr) - 1:
+                return not root.left and not root.right
+            return dfs(root.left, index + 1) or dfs(root.right, index + 1)
+
+        return dfs(root, 0)
