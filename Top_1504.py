2433956f25059914b219c05b09205100e1d1ee0c
AlexSiXiong <2428545784@qq.com>
2020-07-08 09:18:34 +1200

    1504计算有多少个sub矩形

--- Top_1504.py
+++ Top_1504.py
@@ -1,77 +1,69 @@
-from typing import List
+import sys
 
 
-def num_mat(mat: List[List[int]]) -> int:
-    """
-    Q3
-
-    这是一个java根据每一row的解法，但是可解释性不强
-    当处理完row，处理col的时候，没有办法解释面积>1的矩形是怎么计数的（就是+1)
-
-    int m = matrix.length, n = matrix[0].length, res = 0;
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                // matrix stores num of consecutive ones from matrix[i][j] first zero on its right
-                matrix[i][j] = matrix[i][j] == 0 ? 0 : matrix[i][j] + matrix[i][j + 1];
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int min = matrix[i][j];
-                res += min;
-                for (int k = i + 1; k < m; k++) {
-                    if (matrix[k][j] == 0) break;
-                    min = Math.min(min, matrix[k][j]); # 就是这个地方
-                    res += min;
-                }
-            }
-        }
-        return res;
-    """
+def counter_matrix(mat):
     rows = len(mat)
     cols = len(mat[0])
-    res = 0
 
+    width_matrix = [[0] * cols for _ in range(rows)]
+    print(width_matrix)
+    # from left to right
+    for i in range(rows):
+        for j in reversed(range(cols)):
 
-
-    return res
-
-
-def one_row(arr: List[int]) -> int:
-    if len(arr) == 0:
-        return 0
-    counter = 0
-    accumulated_rect = 0
-    for i in arr:
-        if i == 1:
-            accumulated_rect = accumulated_rect + 1  # 这个地方没看懂，为什么要累加
-        else:
-            accumulated_rect = 0
-        counter += accumulated_rect
-    return counter
-
-
-def numSubmat2(self, mat: List[List[int]]) -> int:
-    rows = len(mat)
-    cols = len(mat[0])
-    a = mat
-    ret = 0
-    f = [[0] * cols for _ in range(rows)]
-
-    allonecols = [[0] * cols for _ in range(rows)]
+            if mat[i][j] == 1:
+                # 边界
+                if j != cols - 1:
+                    width_matrix[i][j] = 1 + width_matrix[i][j + 1]
+                else:
+                    width_matrix[i][j] = 1
+    print(width_matrix)
+    result = 0
     for i in range(rows):
         for j in range(cols):
-            if a[i][j] == 1:
-                allonecols[i][j] = (allonecols[i - 1][j] if i > 0 else 0) + 1
-    # print("allonecols", allonecols)
-    for k in range(1, rows + 1):
-        for i in range(rows):
-            for j in range(cols):
-                if allonecols[i][j] >= k:
-                    c = f[i][j] = (f[i][j - 1] if j > 0 else 0) + 1
-                    ret += c
-                else:
-                    f[i][j] = 0
-        # print("f", f)
-    return ret
+            width = sys.maxsize
+            for row in range(i, rows):
+                width = min(width, width_matrix[row][j])
+                result += width
+    print(result)
+    return result
+
+
+def submat(mat):
+    m = len(mat)
+    n = len(mat[0])
+
+    one_counts = [[0] * n for _ in range(m)]
+
+    for i in range(m):
+        for j in range(n - 1, -1, -1):
+            if mat[i][j] == 1:
+                one_counts[i][j] += 1 + (one_counts[i][j + 1] if j < n - 1 else 0)
+
+    ans = 0
+    box = []
+    for i in range(m):
+        for j in range(n):
+            if mat[i][j] == 1:
+                min_w = sys.maxsize
+                for k in range(i, m):
+                    min_w = min(min_w, one_counts[k][j])
+                    # print('k', k, 'j', j)
+
+                    # min_w = one_counts[k][j]
+                    # print(min_w)
+                    box.append(min_w)
+                    ans += min_w
+    print(ans)
+    return ans
+
+
+a = [[0, 1, 1, 0],
+     [0, 1, 1, 1],
+     [1, 1, 1, 0]]
+# submat(a)
+
+b = [1, 1, 1, 1, 0, 0, 2, 2, 1, 1, 2, 1]
+c = [1, 2, 2, 1, 0, 0, 2, 2, 1, 1, 2, 1]
+
+counter_matrix(a)
