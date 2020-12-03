3f14a2bbf14e965af8524a815f300501b260b39a
AlexSiXiong <2428545784@qq.com>
2020-12-03 16:51:09 +1300

    一个stack+括号的复杂版

--- Top_394.py
+++ Top_394.py
@@ -1,4 +1,4 @@
-def decodeString(s: str) -> str:
+def decodeString1(s: str) -> str:
     stack = []
     cur_num = ''
     cur_str = ''
@@ -34,6 +34,29 @@ a3 = "abcabccdcdcdef"
 
 s4 = "abc3[cd]xyz"
 a4 = "abccdcdcdxyz"
+
+
+def decodeString(s: str) -> str:
+    num = ''
+    string = ''
+    stack = []
+
+    for cur in s:
+        if cur == '[':
+            stack.append(int(num))
+            stack.append(string)
+            num = ''
+            string = ''
+        elif cur == ']':
+            pre_s = stack.pop()
+            pre_n = stack.pop()
+            string = pre_s + string * pre_n
+        elif cur.isdigit():
+            num += cur
+        else:
+            string += cur
+
+    return string
 
 assert decodeString(s1) == a1
 assert decodeString(s2) == a2
