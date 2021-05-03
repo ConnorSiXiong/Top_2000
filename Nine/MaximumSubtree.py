200646ceb0c3d2517f846bb2c7626be0f3013a90
Alexander <2428545784@qq.com>
2021-05-03 20:31:56 +1200

    增加了更robust的解法，增加了草稿，二叉树，分治

--- Nine/MaximumSubtree.py
+++ Nine/MaximumSubtree.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -49,7 +52,68 @@ def findSubtree(root):
     return res
 
 
-print(findSubtree(l1))
+findSubtree(l1)
+
 
 # 思考:
 # 这个题怎么写成递归函数两个返回值的写法？
+# 第二天，解决了这个问题, 就是要把那个值给返回回去
+
+
+def findSubtree2(root):
+    max_val, node, max_sum = getSumSubtree(root)
+    print(node.val)
+    return node
+
+
+def getSumSubtree(node):
+    # 叶子结点的下一层
+    if not node:
+        return -sys.maxsize, node, 0
+
+    left_max, left_node, left_sum = getSumSubtree(node.left)
+    right_max, right_node, right_sum = getSumSubtree(node.right)
+
+    cur_sum = left_sum + right_sum + node.val
+
+    if left_max == max(left_max, right_max, cur_sum):
+        return left_max, left_node, cur_sum
+    if right_max == max(right_max, left_max, cur_sum):
+        return right_max, right_node, cur_sum
+    return cur_sum, node, cur_sum
+
+
+findSubtree2(l1)
+
+
+# ------------- 草稿 -----------------
+# -------- 草稿的有更多打印信息 ---------
+
+def findMaxSumSubtree(root):
+    i = 0
+    max_val, root_node, sum_of_tree = getSubtreeSum(root, i)
+    return max_val, root_node.val, sum_of_tree
+
+
+def getSubtreeSum(node, i):
+    if not node:
+        return -sys.maxsize, node, 0
+    i += 1
+    left_max, left_node, left_sum = getSubtreeSum(node.left, i)
+    right_max, right_node, right_sum = getSubtreeSum(node.right, i)
+
+    print('Tree Depth:', i)
+    print('left_sum', left_sum)
+    print('right_sum', right_sum)
+    cur_sum = left_sum + right_sum + node.val
+    print('node.val', node.val)
+    print('cur_sum', cur_sum)
+    print('----------')
+    if left_max == max(left_max, right_max, cur_sum):
+        return left_max, left_node, cur_sum
+    if right_max == max(left_max, right_max, cur_sum):
+        return right_max, right_node, cur_sum
+    return cur_sum, node, cur_sum
+
+
+print(findMaxSumSubtree(l1))
