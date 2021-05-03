96b7d6e043842477099171c288cacce1bdc18ffd
Alexander <2428545784@qq.com>
2021-05-03 20:43:05 +1200

    增加了二分法的一种新写法, 复习

--- Top_704.py
+++ Top_704.py
@@ -29,5 +29,36 @@ a = [-1, 2]
 target0 = -1
 result = search2(a, target0)
 print(result)
+"""
+    使用新办法
+    - 复习
+    2021-05-03
+    20:37 - 20:41
+"""
 
 
+def search3(arr, target):
+    if not arr:
+        return -1
+    if target > arr[-1] or target < arr[0]:
+        return -1
+
+    left = 0
+    right = len(arr)
+
+    while left + 1 < right:
+        mid = (left + right) // 2
+
+        if target < arr[mid]:
+            right = mid
+        else:
+            left = mid
+
+    if arr[left] == target:
+        return left
+    if arr[right] == target:
+        return right
+    return -1
+
+
+print(search3([-1, 0, 3, 5, 9, 12], 2))
