081354bcb1b693f01811c6161aa7cae1f6252988
Alexander <2428545784@qq.com>
2021-05-07 23:38:05 +1200

    add a BFS solution

--- Top_78.py
+++ Top_78.py
@@ -1,3 +1,4 @@
+import collections
 import random
 
 
@@ -22,3 +23,23 @@ def dfs(arr, index, subset, res):
 
 
 print(subsets([1, 2, 3]))
+
+
+def subsets2(nums):
+    nums = sorted(nums)
+    res = []
+    q = [([], 0)]
+    print(q)
+    while q:
+
+        arr, n = q.pop(0)
+        print("arr and n:", arr, n)
+        res.append(arr)
+
+        for i in range(n, len(nums)):
+            q.append((arr + [nums[i]], i + 1))
+
+    return res
+
+
+print(subsets2([1, 2, 3]))
