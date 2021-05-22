8b4509c1e4e94f0dda4a27d8b2ccea59fc27b9f9
Alexander <2428545784@qq.com>
2021-05-23 10:46:57 +1200

    practiced

--- Nine/Ladder1/Week5/Q_120.py
+++ Nine/Ladder1/Week5/Q_120.py
@@ -1,16 +1,21 @@
 def minimumTotal(arr):
-    return divideConquer(arr, 0, 0, {})
+    return dc(arr, 0, 0, {})
 
 
-def divideConquer(arr, x, y, memo):
-    if x == len(arr):
+def dc(arr, x, y, memo):
+    if len(arr) == x:
         return 0
 
     if (x, y) in memo:
         return memo[(x, y)]
 
-    left = divideConquer(arr, x + 1, y, memo)
-    right = divideConquer(arr, x + 1, y + 1, memo)
-    memo[(x, y)] = min(left, right) + arr[x][y]
+    left = dc(arr, x + 1, y, memo)
+    right = dc(arr, x + 1, y + 1, memo)
 
+    memo[(x, y)] = min(left, right) + arr[x][y]
     return memo[(x, y)]
+
+
+triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+
+print(minimumTotal(triangle))
