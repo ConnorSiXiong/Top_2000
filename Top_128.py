bc78efcdffa85718b5ae7aec4d79948ded1d0d55
Alexander <2428545784@qq.com>
2021-06-08 21:10:51 +1200

    come up with another solution

--- Top_128.py
+++ Top_128.py
@@ -23,3 +23,28 @@ nums = [100, 4, 200, 1, 3, 2]
 nums = [1, 2, 0, 1]
 nums = [0, 0, 0, 0, 0, 1]
 print(longestConsecutive(nums))
+
+
+def longestConsecutive2(nums):
+    # 边界考虑
+
+    # 1. 空数组
+    # 2. 一个位置的数组
+    # 3. 相邻重复怎么算？
+
+    # 我上面那个解法是从1开始计数
+    # 这种从1开始计数的这是我第一次做到
+
+    set_nums = set(nums)
+
+    res = 0
+    for i in nums:
+        if i-1 not in set_nums:
+            temp = 1
+            while i + 1 in set_nums:
+                temp += 1
+                i += 1
+            res = max(res, temp)
+    return res
+
+print(longestConsecutive2(nums))
