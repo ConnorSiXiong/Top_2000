943c14698958a8421c945a045e06b0cdb1000478
Alexander <2428545784@qq.com>
2021-06-15 22:49:19 +1200

    classic bfs

---
+++ Top_505.py
@@ -0,0 +1,35 @@
+import heapq
+from typing import List
+
+
+def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+    start, destination = tuple(start), tuple(destination)
+    row_size = len(maze)
+    col_size = len(maze[0])
+
+    def neighbors(maze, node):
+        temp = []
+        used = set()
+        used.add(node)
+        for dx, dy in [(-1, 0), (0, 1), (0, -1), (1, 0)]:
+            (x, y), dist = node, 0
+            while 0 <= x + dx < row_size and 0 <= y + dy < col_size and maze[x + dx][y + dy] == 0:
+                x += dx
+                y += dy
+                dist += 1
+            if (x, y) not in used:
+                temp.append((dist, (x, y)))
+        return temp
+
+    heap = [(0, start)]
+    visited = set()
+    while heap:
+        dist, node = heapq.heappop(heap)
+        if node in visited: continue
+        if node == destination:
+            return dist
+        visited.add(node)
+        for neighbor_dist, neighbor in neighbors(maze, node):
+            heapq.heappush(heap, (dist + neighbor_dist, neighbor))
+
+    return -1
