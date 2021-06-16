84f323ece82f936791a67d92f08542df080ecd53
Alexander <2428545784@qq.com>
2021-06-16 23:36:34 +1200

    优化了队列和剪枝部分

--- Top_505.py
+++ Top_505.py
@@ -1,8 +1,10 @@
 import heapq
 from typing import List
+import numpy as np
+from collections import deque
 
 
-def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+def shortestDistance(maze: List[List[int]], start: List[int], destination: List[int]) -> int:
     start, destination = tuple(start), tuple(destination)
     row_size = len(maze)
     col_size = len(maze[0])
@@ -33,3 +35,64 @@ def shortestDistance(self, maze: List[List[int]], start: List[int], destination:
             heapq.heappush(heap, (dist + neighbor_dist, neighbor))
 
     return -1
+
+
+def shortestDistance2(maze, start, destination) -> int:
+    maze = np.array(maze)
+
+    start = tuple(start)
+    destination = tuple(destination)
+
+    if start == destination:
+        return 0
+
+    visited = set()
+    visited.add(start)
+
+    q = deque([])
+    q.append([start, 0])
+
+    while q:
+        cur_position, steps = q.popleft()
+
+        if cur_position == destination:
+            return steps
+
+        for next_movement in getNextMovement(maze, cur_position, visited):
+            next_position, sub_steps = next_movement
+            q.append([next_position, steps + sub_steps])
+    return -1
+
+
+D = [
+    (1, 0),
+    (-1, 0),
+    (0, 1),
+    (0, -1)
+]
+
+
+def getNextMovement(maze, position, visited):
+    rows = maze.shape[0]
+    cols = maze.shape[1]
+    res = []
+
+    for dx, dy in D:
+        x, y = position
+        sub_steps = 0
+        while 0 <= x + dx < cols and 0 <= y + dy < rows and maze[x + dx, y + dy] == 0:
+            x += dx
+            y += dy
+            sub_steps += 1
+        if (x, y) in visited:
+            continue
+        res.append([(x, y), sub_steps])
+        visited.add((x, y))
+    return res
+
+
+maze = [[0, 0, 1, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [1, 1, 0, 1, 1], [0, 0, 0, 0, 0]]
+start = [0, 4]
+destination = [4, 4]
+
+print(shortestDistance2(maze, start, destination))
