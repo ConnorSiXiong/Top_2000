8a17544d24a0fe80773d4f8921cac2348191ebd4
Alexander <2428545784@qq.com>
2021-08-22 22:03:39 +1200

    draft of the contest 255

--- Draft草稿纸/temp.py
+++ Draft草稿纸/temp.py
@@ -308,269 +308,380 @@ words = [
 #
 # words = {v: k for k, v in dict(words).items()}
 # print(list(words))
+#
+#
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left_weight = '0'
+#         self.right_weight = '1'
+#         self.left = None
+#         self.right = None
+#
+#
+# class HuffmanTree:
+#     def __init__(self, words_tuple):
+#         self.words_tuple = words_tuple
+#         self.words = words_tuple
+#         self.words = self.__init_nodes()
+#         self.build_tree()
+#         self.root = self.words[0][1]
+#         self.get_coding_len()
+#
+#     def __init_nodes(self):
+#         arr = []
+#         for i in range(len(self.words)):
+#             arr.append([self.words[i][1], Node(self.words[i][0])])
+#         return sorted(arr, key=lambda i: i[0], reverse=True)
+#
+#     def build_tree(self):
+#         while len(self.words) != 1:
+#             self.print_keys()
+#             left_set = self.words.pop()
+#             right_set = self.words.pop()
+#             left_node = left_set[1]
+#             right_node = right_set[1]
+#             new_node = Node(left_set[0] + right_set[0])
+#             new_node.left = left_node
+#             new_node.right = right_node
+#             self.words.append([new_node.value, new_node])
+#             self.__sort_words()
+#
+#     def __sort_words(self):
+#         self.words = sorted(self.words, key=lambda i: i[0], reverse=True)
+#
+#     def print_keys(self):
+#         arr = []
+#         for i in self.words:
+#             arr.append(i[0])
+#         print(arr)
+#
+#     def coding_tree(self):
+#         res = []
+#         path = []
+#         self._dfs(self.root, path, res)
+#         return res
+#
+#     def _dfs(self, node, path, res):
+#         if str.isalpha(str(node.value)):
+#             res.append([node.value, ''.join(path[:])])
+#             return
+#
+#         if node.right:
+#             path.append(node.right_weight)
+#             self._dfs(node.right, path, res)
+#             path.pop()
+#         if node.left:
+#             path.append(node.left_weight)
+#             self._dfs(node.left, path, res)
+#             path.pop()
+#
+#     def get_coding_len(self):
+#         frequency = dict(self.words_tuple)
+#         coding_res = dict(self.coding_tree())
+#         # # debug用
+#         # print(frequency)
+#         # print(coding_res)
+#         return sum(len(coding_res[i]) * frequency[i[0]] for i in coding_res.keys())
+#
+#
+# class BinaryTree:
+#     def __init__(self, words_tuple):
+#         self.words_tuple = words_tuple
+#         self.root = self.build_tree()
+#
+#     def build_tree(self):
+#         notes = self.get_all_words()
+#         root = Node(0)
+#         cur_node = root
+#
+#         while notes:
+#             left_node = notes.pop()
+#             cur_node.left = Node(left_node)
+#             if len(notes) == 1:
+#                 cur_node.right = Node(notes.pop())
+#             else:
+#                 cur_node.right = Node(0)
+#             cur_node = cur_node.right
+#         return root
+#
+#     def get_all_words(self):
+#         return [i[0] for i in self.words_tuple]
+#
+#     def coding_tree(self):
+#         res = []
+#         path = []
+#         self._dfs(self.root, path, res)
+#         return res
+#
+#     def _dfs(self, node, path, res):
+#         if str.isalpha(str(node.value)):
+#             res.append([node.value, ''.join(path[:])])
+#             return
+#
+#         if node.right:
+#             path.append(node.right_weight)
+#             self._dfs(node.right, path, res)
+#             path.pop()
+#         if node.left:
+#             path.append(node.left_weight)
+#             self._dfs(node.left, path, res)
+#             path.pop()
+#
+#     def get_coding_len(self):
+#         frequency = dict(self.words_tuple)
+#         coding_res = dict(self.coding_tree())
+#         # # debug用
+#         # print(frequency)
+#         # print(coding_res)
+#         return sum(len(coding_res[i]) * frequency[i[0]] for i in coding_res.keys())
+#
+#
+# b = BinaryTree(words)
+# print(b.get_coding_len())
+#
+# a = []
+#
+# a.append([1, 'haha'])
+# a.append([1000, 'bb'])
+# a.append([3, 'cc'])
+#
+# t = HuffmanTree(words)
+#
+#
+#
+# from collections import deque
+#
+# prerequisites = [
+#     [2, 1],
+#     [2, 0],
+#     [3, 2]
+# ]
+#
+# from collections import defaultdict
+#
+#
+# class Solution:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         edges = defaultdict(list)
+#         indge = [0] * numCourses
+#         res = 0
+#
+#         for i in prerequisites:
+#             edges[i[1]].append(i[0])
+#             indge[i[0]] += 1
+#
+#         q = deque([u for u in range(numCourses) if indge[u] == 0])
+#
+#         while q:
+#             u = q.popleft()
+#             res += 1
+#             for v in edges[u]:
+#                 indge[v] -= 1
+#                 if indge[v] == 0:
+#                     q.append(v)
+#         return res == numCourses
+#
+#
+# a = Solution()
+#
+# a.canFinish(4, prerequisites)
+#
+# """
+# >>> s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+# >>> d = defaultdict(list)
+# >>> d.default_factory
+# <type 'list'>
+# >>> for k, v in s:
+# ...     d[k].append(v)
+# >>> d.items()
+# [('blue', [2, 4]), ('red', [1]), ('yellow', [1, 3])]
+# >>> d
+# defaultdict(<type 'list'>, {'blue': [2, 4], 'red': [1], 'yellow': [1, 3]})
+# """
+#
+# prerequisites = [
+#     [2, 1],
+#     [2, 0],
+#     [3, 2]
+# ]
+#
+# a = Solution()
+#
+# print(a.canFinish(4, prerequisites))
+# class Solution2:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         pre_course_dict = defaultdict(list)
+#         course_prerequisites = [0] * numCourses
+#
+#         for i in prerequisites:
+#             pre_course_dict[i[1]].append(i[0])
+#             course_prerequisites[i[0]] += 1
+#
+#         q = deque([i for i in range(numCourses) if course_prerequisites[i] == 0])
+#         res = 0
+#         while q:
+#             finished = q.popleft()
+#             res += 1
+#             for i in pre_course_dict[finished]:
+#                 course_prerequisites[i] -= 1
+#                 if course_prerequisites[i] == 0:
+#                     q.append(i)
+#         return res == numCourses
+#
+#
+# b = Solution2()
+#
+# print(b.canFinish(4, prerequisites))
+#
+# prerequisites = [
+#     [2, 1],
+#     [2, 0],
+#     [3, 2]
+# ]
+#
+#
+# class Solution3:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         class_dict = defaultdict(list)
+#         lesson_arr = [0] * numCourses
+#
+#         for i in prerequisites:
+#             lesson_arr[i[0]] += 1
+#             class_dict[i[1]].append(i[0])  # 课程加一个前置
+#
+#         q = deque([i for i in range(numCourses) if lesson_arr[i] == 0])  # -> 队列初始化
+#
+#         while q:
+#             cur = q.popleft()
+#
+#             for i in class_dict[cur]:
+#                 lesson_arr[i] -= 1
+#                 if lesson_arr[i] == 0:
+#                     q.append(i)
+#
+#         return sum(lesson_arr) == 0
+#
+#
+#
+#
+#
+#
+# a = Solution3()
+# print(a.canFinish(4, prerequisites))
 
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left_weight = '0'
-        self.right_weight = '1'
-        self.left = None
-        self.right = None
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
-class HuffmanTree:
-    def __init__(self, words_tuple):
-        self.words_tuple = words_tuple
-        self.words = words_tuple
-        self.words = self.__init_nodes()
-        self.build_tree()
-        self.root = self.words[0][1]
-        self.get_coding_len()
+root = TreeNode(3)
+level_1_left = TreeNode(1)
+level_1_right = TreeNode(4)
 
-    def __init_nodes(self):
-        arr = []
-        for i in range(len(self.words)):
-            arr.append([self.words[i][1], Node(self.words[i][0])])
-        return sorted(arr, key=lambda i: i[0], reverse=True)
+level_2_left_left = TreeNode(3)
+level_2_right_left = TreeNode(1)
+level_2_right_right = TreeNode(5)
 
-    def build_tree(self):
-        while len(self.words) != 1:
-            self.print_keys()
-            left_set = self.words.pop()
-            right_set = self.words.pop()
-            left_node = left_set[1]
-            right_node = right_set[1]
-            new_node = Node(left_set[0] + right_set[0])
-            new_node.left = left_node
-            new_node.right = right_node
-            self.words.append([new_node.value, new_node])
-            self.__sort_words()
+root.left = level_1_left
+root.right = level_1_right
 
-    def __sort_words(self):
-        self.words = sorted(self.words, key=lambda i: i[0], reverse=True)
+level_1_left.left = level_2_left_left
+level_1_right.left = level_2_right_left
+level_1_right.right = level_2_right_right
 
-    def print_keys(self):
-        arr = []
-        for i in self.words:
-            arr.append(i[0])
-        print(arr)
+"""
+               3 
+               
+        1             4
+        
+   3               1       5
 
-    def coding_tree(self):
-        res = []
-        path = []
-        self._dfs(self.root, path, res)
-        return res
+"""
 
-    def _dfs(self, node, path, res):
-        if str.isalpha(str(node.value)):
-            res.append([node.value, ''.join(path[:])])
-            return
+#
+# class Solution:
+#     def findGCD(self,  nums: List[int]) -> int:
+#         nums = sorted(nums)
+#         a = nums[0]
+#         b = nums[-1]
+#         return self.hcf(a, b)
+#
+#     def hcf(self, a, b):
+#         c, d = max(a, b), min(a, b)
+#         e = 1
+#         while e != 0:
+#             e = c % d
+#             c, d = d, e
+#         return c
+#
+#
+#
+# a= Solution()
+#
+# print(a.findGCD([2,5,6,9,10]))
 
-        if node.right:
-            path.append(node.right_weight)
-            self._dfs(node.right, path, res)
-            path.pop()
-        if node.left:
-            path.append(node.left_weight)
-            self._dfs(node.left, path, res)
-            path.pop()
+# 有n个长度为n的二进制01
+#
+# class Solution:
+#     def findDifferentBinaryString(self, nums: List[str]) -> str:
+#         n = len(nums)
+#
+#         start_index = 0
+#         change_index = start_index
+#         temp = ["1"] * n
+#         while 1:
+#             if ''.join(temp) not in nums:
+#                 return ''.join(temp)
+#             temp[change_index] = '0'
+#
+#             change_index += 1
+#             if change_index == n+1:
+#                 start_index += 1
+#                 change_index = start_index
+#
+#
+#
+#
+#     def dfs(self, n, index, comb, box):
+#         if len(comb) == n:
+#             box.append(''.join(comb[:]))
+#             return
+#         for i in range(index, n):
+#             comb.append('1')
+#             self.dfs(n, i+1, comb[:], box)
+#             comb.pop()
+#             comb.append('0')
+#             self.dfs(n, i+1, comb, box)
+#
+# # 最多只有16种组合
+# # 我只用找到其中某一位置都没出现的都行
+#
+# a = Solution()
+#
+# print(a.findDifferentBinaryString(['111','110','000']))
+#
+# # print(set(sorted(["111","011","001"])))
+#
+# """
+# "00001101111011","11011001010101","00001000000001","00011100000000","01101111001010","01011011010101","00110100111111","10001100101110","01000000010011","00110000101011","01101010000101","10000111010100","11000000111000","00101101110110"]
+# """
 
-    def get_coding_len(self):
-        frequency = dict(self.words_tuple)
-        coding_res = dict(self.coding_tree())
-        # # debug用
-        # print(frequency)
-        # print(coding_res)
-        return sum(len(coding_res[i]) * frequency[i[0]] for i in coding_res.keys())
+import numpy as np
 
-
-class BinaryTree:
-    def __init__(self, words_tuple):
-        self.words_tuple = words_tuple
-        self.root = self.build_tree()
-
-    def build_tree(self):
-        notes = self.get_all_words()
-        root = Node(0)
-        cur_node = root
-
-        while notes:
-            left_node = notes.pop()
-            cur_node.left = Node(left_node)
-            if len(notes) == 1:
-                cur_node.right = Node(notes.pop())
-            else:
-                cur_node.right = Node(0)
-            cur_node = cur_node.right
-        return root
-
-    def get_all_words(self):
-        return [i[0] for i in self.words_tuple]
-
-    def coding_tree(self):
-        res = []
-        path = []
-        self._dfs(self.root, path, res)
-        return res
-
-    def _dfs(self, node, path, res):
-        if str.isalpha(str(node.value)):
-            res.append([node.value, ''.join(path[:])])
-            return
-
-        if node.right:
-            path.append(node.right_weight)
-            self._dfs(node.right, path, res)
-            path.pop()
-        if node.left:
-            path.append(node.left_weight)
-            self._dfs(node.left, path, res)
-            path.pop()
-
-    def get_coding_len(self):
-        frequency = dict(self.words_tuple)
-        coding_res = dict(self.coding_tree())
-        # # debug用
-        # print(frequency)
-        # print(coding_res)
-        return sum(len(coding_res[i]) * frequency[i[0]] for i in coding_res.keys())
-
-
-b = BinaryTree(words)
-print(b.get_coding_len())
-
-a = []
-
-a.append([1, 'haha'])
-a.append([1000, 'bb'])
-a.append([3, 'cc'])
-
-t = HuffmanTree(words)
-
-
-
-from collections import deque
-
-prerequisites = [
-    [2, 1],
-    [2, 0],
-    [3, 2]
-]
-
-from collections import defaultdict
+mat = [[3, 2, 1], [40, 5, 6], [7, 8, 9]]
 
 
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        edges = defaultdict(list)
-        indge = [0] * numCourses
-        res = 0
+    def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
+        for row in mat:
+            row = sorted(row)
 
-        for i in prerequisites:
-            edges[i[1]].append(i[0])
-            indge[i[0]] += 1
-
-        q = deque([u for u in range(numCourses) if indge[u] == 0])
-
-        while q:
-            u = q.popleft()
-            res += 1
-            for v in edges[u]:
-                indge[v] -= 1
-                if indge[v] == 0:
-                    q.append(v)
-        return res == numCourses
+        return 0
 
 
 a = Solution()
-
-a.canFinish(4, prerequisites)
-
-"""
->>> s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
->>> d = defaultdict(list)
->>> d.default_factory
-<type 'list'>
->>> for k, v in s:
-...     d[k].append(v)
->>> d.items()
-[('blue', [2, 4]), ('red', [1]), ('yellow', [1, 3])]
->>> d
-defaultdict(<type 'list'>, {'blue': [2, 4], 'red': [1], 'yellow': [1, 3]})
-"""
-
-prerequisites = [
-    [2, 1],
-    [2, 0],
-    [3, 2]
-]
-
-a = Solution()
-
-print(a.canFinish(4, prerequisites))
-class Solution2:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        pre_course_dict = defaultdict(list)
-        course_prerequisites = [0] * numCourses
-
-        for i in prerequisites:
-            pre_course_dict[i[1]].append(i[0])
-            course_prerequisites[i[0]] += 1
-
-        q = deque([i for i in range(numCourses) if course_prerequisites[i] == 0])
-        res = 0
-        while q:
-            finished = q.popleft()
-            res += 1
-            for i in pre_course_dict[finished]:
-                course_prerequisites[i] -= 1
-                if course_prerequisites[i] == 0:
-                    q.append(i)
-        return res == numCourses
-
-
-b = Solution2()
-
-print(b.canFinish(4, prerequisites))
-
-prerequisites = [
-    [2, 1],
-    [2, 0],
-    [3, 2]
-]
-
-
-class Solution3:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        class_dict = defaultdict(list)
-        lesson_arr = [0] * numCourses
-
-        for i in prerequisites:
-            lesson_arr[i[0]] += 1
-            class_dict[i[1]].append(i[0])  # 课程加一个前置
-
-        q = deque([i for i in range(numCourses) if lesson_arr[i] == 0])  # -> 队列初始化
-
-        while q:
-            cur = q.popleft()
-
-            for i in class_dict[cur]:
-                lesson_arr[i] -= 1
-                if lesson_arr[i] == 0:
-                    q.append(i)
-
-        return sum(lesson_arr) == 0
-
-
-
-
-
-
-a = Solution3()
-print(a.canFinish(4, prerequisites))
-
-
-
-
-
+a.minimizeTheDifference(mat, 10)
