04a5d3f352cc66ff088da1c80e10a0525b3d4f90
Alexander <2428545784@qq.com>
2021-08-29 17:27:05 +1200

    add a solution based on QS

--- Top_215.py
+++ Top_215.py
@@ -4,7 +4,7 @@ import random
 
 def findKthLargest(nums: List[int], k: int) -> int:
     def partition(arr: List[int], left: int, right: int) -> int:
-        index = random.randint(right-left, right)
+        index = random.randint(0, right-left) + left
         temp = arr[index]
         arr[index] = arr[left]
         arr[left] = temp
@@ -35,3 +35,23 @@ def findKthLargest(nums: List[int], k: int) -> int:
             left = index + 1
         elif index > target:
             right = index - 1
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return None
+        pivot = random.choice(nums)
+
+        left = [i for i in nums if i < pivot]
+        mid = [i for i in nums if i == pivot]
+        right = [i for i in nums if i > pivot]
+
+        r = len(right)
+        m = len(mid)
+
+        if k <= r:
+            return self.findKthLargest(right, k)
+        elif k > r + m:
+            return self.findKthLargest(left, k-r-m)
+        else:
+            return mid[0]
